c2bb55a1c768ed419eac658d14c733a773d85457
lalyos <lalyos@yahoo.com>
2025-03-11 14:33:58 +0100

    Enhance start.sh to generate a complete HTML document with customizable title, color, and body content

--- start.sh
+++ start.sh
@@ -1,4 +1,16 @@
 #!/bin/bash
 
-echo ${TITLE:-Welcome} > /var/www/html/index.html
+cat > /var/www/html/index.html <<EOF
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>Document</title>
+</head>
+<body bgcolor="${COLOR:-yellow}">
+    <h1>${TITLE:-Welcome}</h1>
+    ${BODY:-Please use TITLE/COLOR/BODY env vars}
+</body>
+</html>
+EOF
+
 nginx -g "daemon off;"
